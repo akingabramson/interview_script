268e20099c756f6e4cabdf3005a43344e8399fef
Asher King Abramson <a.kingabramson@gmail.com>
2013-08-15 16:16:27 -0700

    inital commit

---
+++ interview.sh
@@ -0,0 +1,4 @@
+open /Applications/Skype.app &
+open /Applications/Google\ Chrome.app "https://code.stypi.com/ashpacademy/Cats" &
+open /Applications/Google\ Chrome.app "https://github.com/appacademy/instructors/blob/master/interview-process.md" &
+open /Applications/Google\ Chrome.app "http://www.appacademy.io/applications/hud" &
